2cef2a9b08c814735caf7a621cbcc9910714d807
salvoab <salvatore.abate85@gmail.com>
2021-01-12 13:28:54 +0100

    Milestone 1 completata
    Visualizzate le info di base richieste

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,21 @@
 let app = new Vue({
     el: '#app',
     data: {
-        movies: []
+        movies: [],
+        searchInput: ""
     },
     methods: {
-        
+        search(){
+            if(this.searchInput === ""){
+                this.movies = [];
+            } else {
+                const query = encodeURI(this.searchInput);
+                const url = `https://api.themoviedb.org/3/search/movie?api_key=027db1a08822b62e35522e7cae42f3bf&language=it-IT&query=${query}`;
+                axios.get(url)
+                    .then(response => this.movies = response.data.results)
+                    .catch(error => console.log(error));
+                
+            }
+        }
     }
 });
